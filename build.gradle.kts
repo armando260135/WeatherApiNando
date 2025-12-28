261666df5ca9500256df98f6f053d84f302c8765
Armando Pineda Paredes <72154277+armando260135@users.noreply.github.com>
2025-12-28 17:23:35 -0500

    add comments for lib (#7)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // --- [ ANDROID & KOTLIN CORE ] ---
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    // --- [ UI & COMPOSE ] ---
     alias(libs.plugins.kotlin.compose) apply false
+
+    // --- [ DATA & UTILITIES ] ---
     alias(libs.plugins.kotlin.parcelize) apply false
+
+    // --- [ DEPENDENCY INJECTION ] ---
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.hilt.android) apply false
 }
